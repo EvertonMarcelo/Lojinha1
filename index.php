2f3c38ae5222c61bace2539ed5188a4b99264cea
EvertonMarcelo <marcelo_everton1@hotmail.com>
2014-06-04 21:07:33 -0300

    Adicionei imagens a home e melhorei o layout da página

---
+++ index.php
@@ -0,0 +1,39 @@
+<html>
+	<head>
+		<meta charset='utf-8'>
+		<link rel="stylesheet" href="estilo.css" type="text/css">
+	</head>
+	<body>
+		<div id=cabecalho>
+		    <?php include "cabecalho.inc";?>
+		</div>
+		<div id=menuesquerda>
+		    <?php include "menuesquerda.inc";?>
+		</div>
+		<div id=conteudo>
+			<table id="tabela" class="stripy_table"  border="0">
+        <tr>
+          <td><img src="1CAERULEA.jpg"  width="280" height="200"></td>
+          <td><img src="2MELANCIABABY.gif "width="280" height="200"></td>
+          <td><img src="3MOSIL.jpg" width="280" height="200"></td>
+        </tr>
+        <tr>
+          <td><img src="4PIMENTAO_PURPLE_BEAUTY.gif" width="280" height="200"></td>
+          <td><img src="5ABOBORA_QUEENSLAND_BLUE.jpg" width="280" height="200"></td>
+          <td><img src="6ABOBRINHA_LIMAO.gif" width="280" height="200"></td>
+        </tr>
+        <tr>
+          <td><img src="7SUZANA_AMARELA.jpg" width="280" height="200"></td>
+          <td><img src="8SUZANA_DOS_OLHOS_NEGROS_BRANCA.jpg" width="280" height="200"></td>
+          <td><img src="9SALVIA_LEUCANTHA.gif" width="280" height="200"></td>
+        </tr>
+        <tr>
+          <td><img src="10NSEN.gif" width="280" height="200"></td>
+          <td><img src="11HABCHOC.jpg" width="280" height="200"></td>
+          <td><img src="12PIMENTA_LEMON_DROP.jpg" width="280" height="200"></td>
+        </tr>
+      </table>
+      <div class="footer" > Desenvolvido por Everton & Nathan</div>
+  		</div>
+	</body>
+</html>
